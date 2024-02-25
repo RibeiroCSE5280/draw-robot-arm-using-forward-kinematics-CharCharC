3711062863529c04e866a2281ea88543f1f443e4
CharCharC <charchar.collins@gmail.com>
2024-02-25 18:55:12 -0500

    Commented Out Animation

--- robot3D_basic.py
+++ robot3D_basic.py
@@ -147,161 +147,161 @@ def forward_kinematics(Phi, L1, L2, L3, L4):
     return T_01, T_02, T_03, T_04, e
 
 # Gets executed by the timer
-# def handle_timer(event):
-#     global T_01, T_02, T_03, T_04, counter1, counter2, counter3, images, num_frames
-#     # Only animate 50 frames
-#     if num_frames < 50:
-#       # Revert each mesh to its original position
-#       Frame1.apply_transform(np.linalg.inv(T_01))
-#       Frame2.apply_transform(np.linalg.inv(T_02))
-#       Frame3.apply_transform(np.linalg.inv(T_03))
-#       end_effector.apply_transform(np.linalg.inv(T_04))
+def handle_timer(event):
+    global T_01, T_02, T_03, T_04, counter1, counter2, counter3, images, num_frames
+    # Only animate 50 frames
+    if num_frames < 50:
+      # Revert each mesh to its original position
+      Frame1.apply_transform(np.linalg.inv(T_01))
+      Frame2.apply_transform(np.linalg.inv(T_02))
+      Frame3.apply_transform(np.linalg.inv(T_03))
+      end_effector.apply_transform(np.linalg.inv(T_04))
 
-#       # Determines new angles
-#       if -90 + counter1 < -45:
-#         phi2 = [-90 + counter1,	120 + counter2,	160 + counter3,	0]
-#         counter1 += 1
-#         counter2 += 3
-#         counter3 += 5
-#       else:
-#           phi2 = [-90 + counter1,	120 + counter2,	160 + counter3,	0]
-#           counter2 += 3
-#           counter3 += 5
-#       L1 = 5
-#       L2 = 8
-#       L3 = 3
-#       L4 = 0
+      # Determines new angles
+      if -90 + counter1 < -45:
+        phi2 = [-90 + counter1,	120 + counter2,	160 + counter3,	0]
+        counter1 += 1
+        counter2 += 3
+        counter3 += 5
+      else:
+          phi2 = [-90 + counter1,	120 + counter2,	160 + counter3,	0]
+          counter2 += 3
+          counter3 += 5
+      L1 = 5
+      L2 = 8
+      L3 = 3
+      L4 = 0
 
-#       # Calculates new transformation matrices
-#       T_01, T_02, T_03, T_04, e = forward_kinematics(phi2, L1, L2, L3, L4)
-#       Frame1.apply_transform(T_01)
-#       Frame2.apply_transform(T_02)
-#       Frame3.apply_transform(T_03)
-#       end_effector.apply_transform(T_04)
-#       plotter.render()
-#       # Takes a screenshot for the gif
-#       # images.append(ImageGrab.grab(bbox = (10,50,1000,800)))
-#       num_frames += 1
-
-
-# # def main():
-
-#   # Set the limits of the graph x, y, and z ranges 
-# axes = Axes(xrange=(0,20), yrange=(-2,10), zrange=(0,6))
-
-# # Lengths of arm parts 
-# L1 = 5   # Length of link 1
-# L2 = 8   # Length of link 2
-# L3 = 3   # Length of link 3
-# L4 = 0   # Length of link 4
-
-# # Joint angles 
-# phi = [-90,	120, 160,	0]
-# # Arm radius
-# radius = .4
-
-# # Stores the images for the gif
-# images = []
-
-# # Starting matrices
-# T_01, T_02, T_03, T_04, e = forward_kinematics(phi, L1, L2, L3, L4)
-
-# # Link 1
-# # Create the coordinate frame mesh and transform
-# Frame1Arrows = createCoordinateFrameMesh()
-
-# # Now, let's create a cylinder and add it to the local coordinate frame
-# link1_mesh = Cylinder(r=0.4, 
-#                       height=L1, 
-#                       pos = ((L1/2)+radius,0,0),
-#                       c="red", 
-#                       alpha=.8, 
-#                       axis=(1,0,0)
-#                       )
-
-# # Also create a sphere to show as an example of a joint
-# sphere1 = Sphere(r = radius).pos(0,0,0).color("gray").alpha(.8)
-
-# # Combine all parts into a single object 
-# Frame1 = Frame1Arrows + link1_mesh + sphere1
-
-# # Transform the part to position it at its correct location and orientation 
-# Frame1.apply_transform(T_01)  
+      # Calculates new transformation matrices
+      T_01, T_02, T_03, T_04, e = forward_kinematics(phi2, L1, L2, L3, L4)
+      Frame1.apply_transform(T_01)
+      Frame2.apply_transform(T_02)
+      Frame3.apply_transform(T_03)
+      end_effector.apply_transform(T_04)
+      plotter.render()
+      # Takes a screenshot for the gif
+      # images.append(ImageGrab.grab(bbox = (10,50,1000,800)))
+      num_frames += 1
 
 
-# # Link 2
-# # Create the coordinate frame mesh and transform
-# Frame2Arrows = createCoordinateFrameMesh()
+# def main():
 
-# # Now, let's create a cylinder and add it to the local coordinate frame
-# link2_mesh = Cylinder(r=0.4, 
-#                       height=L2, 
-#                       pos = ((L2/2)+radius,0,0),
-#                       c="red", 
-#                       alpha=.8, 
-#                       axis=(1,0,0)
-#                       )
+  # Set the limits of the graph x, y, and z ranges 
+axes = Axes(xrange=(0,20), yrange=(-2,10), zrange=(0,6))
 
-# # Also create a sphere to show as an example of a joint
-# sphere2 = Sphere(r = radius).pos(0,0,0).color("gray").alpha(.8)
+# Lengths of arm parts 
+L1 = 5   # Length of link 1
+L2 = 8   # Length of link 2
+L3 = 3   # Length of link 3
+L4 = 0   # Length of link 4
 
-# # Combine all parts into a single object 
-# Frame2 = Frame2Arrows + link2_mesh + sphere2
+# Joint angles 
+phi = [-90,	120, 160,	0]
+# Arm radius
+radius = .4
 
-# # Transform the part to position it at its correct location and orientation 
-# Frame2.apply_transform(T_02)  
+# Stores the images for the gif
+images = []
+
+# Starting matrices
+T_01, T_02, T_03, T_04, e = forward_kinematics(phi, L1, L2, L3, L4)
+
+# Link 1
+# Create the coordinate frame mesh and transform
+Frame1Arrows = createCoordinateFrameMesh()
+
+# Now, let's create a cylinder and add it to the local coordinate frame
+link1_mesh = Cylinder(r=0.4, 
+                      height=L1, 
+                      pos = ((L1/2)+radius,0,0),
+                      c="red", 
+                      alpha=.8, 
+                      axis=(1,0,0)
+                      )
+
+# Also create a sphere to show as an example of a joint
+sphere1 = Sphere(r = radius).pos(0,0,0).color("gray").alpha(.8)
+
+# Combine all parts into a single object 
+Frame1 = Frame1Arrows + link1_mesh + sphere1
+
+# Transform the part to position it at its correct location and orientation 
+Frame1.apply_transform(T_01)  
 
 
-# # Link 3
-# # Create the coordinate frame mesh and transform
-# Frame3Arrows = createCoordinateFrameMesh()
+# Link 2
+# Create the coordinate frame mesh and transform
+Frame2Arrows = createCoordinateFrameMesh()
 
-# # Now, let's create a cylinder and add it to the local coordinate frame
-# link3_mesh = Cylinder(r=0.4, 
-#                       height=L3, 
-#                       pos = ((L3/2)+radius,0,0),
-#                       c="red", 
-#                       alpha=.8, 
-#                       axis=(1,0,0)
-#                       )
+# Now, let's create a cylinder and add it to the local coordinate frame
+link2_mesh = Cylinder(r=0.4, 
+                      height=L2, 
+                      pos = ((L2/2)+radius,0,0),
+                      c="red", 
+                      alpha=.8, 
+                      axis=(1,0,0)
+                      )
 
-# # Also create a sphere to show as an example of a joint
-# sphere3 = Sphere(r = radius).pos(0,0,0).color("gray").alpha(.8)
+# Also create a sphere to show as an example of a joint
+sphere2 = Sphere(r = radius).pos(0,0,0).color("gray").alpha(.8)
 
-# # Combine all parts into a single object 
-# Frame3 = Frame3Arrows + link3_mesh + sphere3
+# Combine all parts into a single object 
+Frame2 = Frame2Arrows + link2_mesh + sphere2
 
-# # Transform the part to position it at its correct location and orientation 
-# Frame3.apply_transform(T_03)  
+# Transform the part to position it at its correct location and orientation 
+Frame2.apply_transform(T_02)  
 
 
-# # End Effector
-# # Create the coordinate frame mesh and transform
-# end_effector = createCoordinateFrameMesh()
+# Link 3
+# Create the coordinate frame mesh and transform
+Frame3Arrows = createCoordinateFrameMesh()
 
-# # Transform the part to position it at its correct location and orientation 
-# end_effector.apply_transform(T_04)
+# Now, let's create a cylinder and add it to the local coordinate frame
+link3_mesh = Cylinder(r=0.4, 
+                      height=L3, 
+                      pos = ((L3/2)+radius,0,0),
+                      c="red", 
+                      alpha=.8, 
+                      axis=(1,0,0)
+                      )
 
-# # Used to update the angles for the animation
-# counter1 = 1
-# counter2 = 3
-# counter3 = 5
+# Also create a sphere to show as an example of a joint
+sphere3 = Sphere(r = radius).pos(0,0,0).color("gray").alpha(.8)
 
-# # Counts the number of frames in the animation
-# num_frames = 0
+# Combine all parts into a single object 
+Frame3 = Frame3Arrows + link3_mesh + sphere3
 
-# # Creates the animation
-# plotter = Plotter(size=(700, 500))
+# Transform the part to position it at its correct location and orientation 
+Frame3.apply_transform(T_03)  
+
+
+# End Effector
+# Create the coordinate frame mesh and transform
+end_effector = createCoordinateFrameMesh()
+
+# Transform the part to position it at its correct location and orientation 
+end_effector.apply_transform(T_04)
+
+# Used to update the angles for the animation
+counter1 = 1
+counter2 = 3
+counter3 = 5
+
+# Counts the number of frames in the animation
+num_frames = 0
+
+# Creates the animation
+plotter = Plotter(size=(700, 500))
 # plotter.initialize_interactor()
-# timer_id = plotter.timer_callback("start", dt=100)
-# timerevt = plotter.add_callback('timer', handle_timer, enable_picking=True)
-# plotter.show([Frame1, Frame2, Frame3, end_effector], axes, viewup="z")
+timer_id = plotter.timer_callback("start", dt=100)
+timerevt = plotter.add_callback('timer', handle_timer, enable_picking=True)
+plotter.show([Frame1, Frame2, Frame3, end_effector], axes, viewup="z")
 
-# # Saves the frames into a gif
-# # if num_frames > 40:
-# #   images[0].save('robot.gif', 
-# #                 save_all = True, append_images = images[1:],  
-# #                 optimize = False, duration = 10, loop=0)
+# Saves the frames into a gif
+# if num_frames > 40:
+#   images[0].save('robot.gif', 
+#                 save_all = True, append_images = images[1:],  
+#                 optimize = False, duration = 10, loop=0)
 
 
 
